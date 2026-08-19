c33658a8b5a33d6fdefede7eb1cf1957e7cb47e9
Sanskar <sanskarin@outlook.in>
2026-08-19 09:01:31 +0530

    feat: add Room database

---
+++ app/src/main/java/in/sanskar/spendcalc/data/local/SpendCalcDatabase.kt
@@ -0,0 +1,14 @@
+package in.sanskar.spendcalc.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [HistoryEntity::class, TemplateEntity::class],
+    version = 1,
+    exportSchema = true,
+)
+abstract class SpendCalcDatabase : RoomDatabase() {
+    abstract fun historyDao(): HistoryDao
+    abstract fun templateDao(): TemplateDao
+}
