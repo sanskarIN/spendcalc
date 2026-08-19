1157143af3097cfe353095b7c6f60478b85b2e39
Sanskar <sanskarin@outlook.in>
2026-08-19 11:34:15 +0530

    feat: install AndroidX launch splash screen

--- app/src/main/java/in/sanskar/spendcalc/MainActivity.kt
+++ app/src/main/java/in/sanskar/spendcalc/MainActivity.kt
@@ -3,6 +3,7 @@ package `in`.sanskar.spendcalc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import `in`.sanskar.spendcalc.ui.SpendCalcApp
@@ -10,6 +11,7 @@ import `in`.sanskar.spendcalc.ui.SpendCalcViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
