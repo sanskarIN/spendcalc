08cbcedae64cb6bd6b0e21c970d0bea443db9a1b
Sanskar <sanskarin@outlook.in>
2026-08-19 09:02:33 +0530

    feat: add reusable design tokens

---
+++ app/src/main/java/in/sanskar/spendcalc/ui/theme/DesignTokens.kt
@@ -0,0 +1,19 @@
+package in.sanskar.spendcalc.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object SpendCalcTokens {
+    val SpaceXs = 4.dp
+    val SpaceSm = 8.dp
+    val SpaceMd = 16.dp
+    val SpaceLg = 24.dp
+    val SpaceXl = 32.dp
+
+    val CornerSm = 8.dp
+    val CornerMd = 16.dp
+    val CornerLg = 24.dp
+
+    val MinTouchTarget = 48.dp
+    val ContentMaxWidth = 920.dp
+    val FormMaxWidth = 640.dp
+}
