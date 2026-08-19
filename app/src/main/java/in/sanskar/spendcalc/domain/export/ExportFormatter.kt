4a400559cdb9816f670c230a7954a9b609a0462e
Sanskar <sanskarin@outlook.in>
2026-08-19 09:03:46 +0530

    feat: add calculation export abstraction

---
+++ app/src/main/java/in/sanskar/spendcalc/domain/export/ExportFormatter.kt
@@ -0,0 +1,14 @@
+package in.sanskar.spendcalc.domain.export
+
+import in.sanskar.spendcalc.domain.model.CalculationInput
+import in.sanskar.spendcalc.domain.model.CalculationResult
+
+interface ExportFormatter {
+    val mimeType: String
+    val fileExtension: String
+
+    fun format(
+        input: CalculationInput,
+        result: CalculationResult,
+    ): String
+}
