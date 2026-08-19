332118d8731f7aa9e2d8ef52437d4bcb2f6aa404
Sanskar <sanskarin@outlook.in>
2026-08-19 09:09:25 +0530

    feat: add Compose main activity

---
+++ app/src/main/java/in/sanskar/spendcalc/MainActivity.kt
@@ -0,0 +1,24 @@
+package in.sanskar.spendcalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import in.sanskar.spendcalc.ui.SpendCalcApp
+import in.sanskar.spendcalc.ui.SpendCalcViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        val container = (application as SpendCalcApplication).container
+        setContent {
+            val spendCalcViewModel: SpendCalcViewModel = viewModel(
+                factory = SpendCalcViewModel.factory(container),
+            )
+            SpendCalcApp(spendCalcViewModel)
+        }
+    }
+}
