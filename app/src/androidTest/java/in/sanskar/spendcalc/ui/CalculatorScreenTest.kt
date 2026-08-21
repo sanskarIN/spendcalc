812d5ed5d6fb69ff2fb9cde032c109ad226d8ee0
Sanskar <sanskarin@outlook.in>
2026-08-21 06:31:59 +0530

    test: fix calculator Compose rule API imports

--- app/src/androidTest/java/in/sanskar/spendcalc/ui/CalculatorScreenTest.kt
+++ app/src/androidTest/java/in/sanskar/spendcalc/ui/CalculatorScreenTest.kt
@@ -1,11 +1,9 @@
 package `in`.sanskar.spendcalc.ui
 
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
